78548e79b05237e2ab197fb9d73dc2f6242dcebc
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-09-25 14:53:28 +0200

    9/25/2025

--- Section_1/02-dev-tools-demo/src/main/java/com/keldorn/mycoolapp/rest/FunRestController.java
+++ Section_1/02-dev-tools-demo/src/main/java/com/keldorn/mycoolapp/rest/FunRestController.java
@@ -1,17 +1,28 @@
 package com.keldorn.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-    // expose "/" that return "Hello World"
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: %s, Team name: %s".formatted(coachName, teamName);
+    }
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
     }
 
-    // expose a new endpoint for "workout"
     @GetMapping("/workout")
     public String getDailyWorkout() {
         return "Run a hard 5k!";
