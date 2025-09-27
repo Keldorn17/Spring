097138e6bef3a2d08951060c501852396c8cc045
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-09-27 22:39:03 +0200

    9/27/2025 - setup

---
+++ Section_3/src/test/java/com/keldorn/cruddemo/Section3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keldorn.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
