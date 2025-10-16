c5044c90ad7660bb6900b20c739fd7773dd169b0
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-10-16 17:44:26 +0200

    10/16/2025 - Spring MVC

---
+++ Section_6/ThymeleafDemo/src/main/java/com/keldorn/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.keldorn.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/showForm")
+    public String shwForm() {
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        String name = request.getParameter("studentName").toUpperCase();
+        model.addAttribute("message", "YO! %s".formatted(name));
+        return "helloworld";
+    }
+}
