317abc1866f4762e590f08bbb3a8a7516264e584
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-10-17 12:00:52 +0200

    10/17/2025 - Student

--- Section_6/ThymeleafDemo/src/main/java/com/keldorn/thymeleafdemo/controller/HelloWorldController.java
+++ Section_6/ThymeleafDemo/src/main/java/com/keldorn/thymeleafdemo/controller/HelloWorldController.java
@@ -3,33 +3,29 @@ package com.keldorn.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping("/showForm")
+    @GetMapping("/processForm")
     public String shwForm() {
-        return "helloworld-form";
+        return "helloworld/helloworld-form";
     }
 
-    @RequestMapping("/processForm")
-    public String processForm() {
-        return "helloworld";
-    }
-
-    @RequestMapping("/processFormVersionThree")
+    @PostMapping("/processForm")
     public String processFormVersionThree(@RequestParam("studentName") String name, Model model) {
         model.addAttribute("message",
                 "Hey My Friend from v3! %s".formatted(name.toUpperCase()));
-        return "helloworld";
+        return "helloworld/helloworld";
     }
 
-    @RequestMapping("/processFormVersionTwo")
+    @PostMapping("/processFormVersionTwo")
     public String letsShoutDude(HttpServletRequest request, Model model) {
         String name = request.getParameter("studentName").toUpperCase();
         model.addAttribute("message", "YO! %s".formatted(name));
-        return "helloworld";
+        return "helloworld/helloworld";
     }
 }
---
+++ Section_6/ThymeleafDemo/src/main/java/com/keldorn/thymeleafdemo/model/Student.java
@@ -0,0 +1,12 @@
+package com.keldorn.thymeleafdemo.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Student {
+
+    private String firstName;
+    private String lastName;
+}
