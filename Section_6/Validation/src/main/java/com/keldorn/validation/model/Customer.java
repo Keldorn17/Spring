f12bddae67e4c05a6a66e97730b3e57a70c484e9
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-10-21 21:11:38 +0200

    10/21/2025 - Messages

--- Section_6/Validation/src/main/java/com/keldorn/validation/model/Customer.java
+++ Section_6/Validation/src/main/java/com/keldorn/validation/model/Customer.java
@@ -14,9 +14,10 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String lastName;
 
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be greater then or equal to zero")
     @Max(value = 10, message = "must be less then or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 characters/digits")
     private String postalCode;
