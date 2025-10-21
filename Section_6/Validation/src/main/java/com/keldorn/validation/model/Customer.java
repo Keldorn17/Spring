05b0a98575dc387bfe23a6a2e7b3f5f711eaa2c0
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-10-21 15:20:24 +0200

    10/20/2025 - InitBinder

---
+++ Section_6/Validation/src/main/java/com/keldorn/validation/model/Customer.java
@@ -0,0 +1,17 @@
+package com.keldorn.validation.model;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Customer {
+
+    private String firstName;
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String lastName;
+}
