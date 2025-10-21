9ea69cf9754b202dea0990af9f1ab87f36a82c96
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-10-21 15:50:14 +0200

    10/21/2025 - Pattern

--- Section_6/Validation/src/main/java/com/keldorn/validation/controller/CustomerController.java
+++ Section_6/Validation/src/main/java/com/keldorn/validation/controller/CustomerController.java
@@ -24,7 +24,6 @@ public class CustomerController {
 
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult bindingResult) {
-        System.out.printf("Last name: |%s|", customer.getLastName());
         if (bindingResult.hasErrors()) {
             return "customer-form";
         }
--- Section_6/Validation/src/main/java/com/keldorn/validation/model/Customer.java
+++ Section_6/Validation/src/main/java/com/keldorn/validation/model/Customer.java
@@ -1,7 +1,6 @@
 package com.keldorn.validation.model;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,4 +13,11 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @Min(value = 0, message = "must be greater then or equal to zero")
+    @Max(value = 10, message = "must be less then or equal to 10")
+    private int freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 characters/digits")
+    private String postalCode;
 }
